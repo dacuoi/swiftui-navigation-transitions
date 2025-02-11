f96d561d976cbf0c997e6952ea74f7ad963639a2
dacuoi <paintbsd@gmail.com>
2025-02-11 12:01:47 +0700

    Update NavigationTransition+SwiftUI.swift

--- Sources/NavigationTransitions/NavigationTransition+SwiftUI.swift
+++ Sources/NavigationTransitions/NavigationTransition+SwiftUI.swift
@@ -5,14 +5,15 @@ extension View {
 	@MainActor
 	public func navigationTransition(
 		_ transition: AnyNavigationTransition,
-		interactivity: AnyNavigationTransition.Interactivity = .default
+		interactivity: AnyNavigationTransition.Interactivity = .default,
+		maxWidthTouch: CGFloat = 0
 	) -> some View {
 		self.introspect(
 			.navigationView(style: .stack),
 			on: .iOS(.v13...), .tvOS(.v13...), .visionOS(.v1...),
 			scope: [.receiver, .ancestor]
 		) { controller in
-			controller.setNavigationTransition(transition, interactivity: interactivity)
+			controller.setNavigationTransition(transition, interactivity: interactivity, maxWidthTouch: maxWidthTouch)
 		}
 	}
 }
